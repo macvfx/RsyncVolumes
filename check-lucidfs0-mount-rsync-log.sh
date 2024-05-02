705b34514c52d611dad5373ac90d12917b7386f2
Mat X <macvfx@users.noreply.github.com>
2024-05-02 10:44:43 -0700

    Create check-lucidfs0-mount-rsync-log.sh
    Rsync LucidLink volume example with logging

---
+++ check-lucidfs0-mount-rsync-log.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check if LucidLink FileSpace is mounted
+# Use rsync to copy everything EXCEPT large movie files. 
+# 
+# Note 1: Edit rsync exclude as needed
+#
+# Note 2: Edit name of LucidLink FileSpace and destination for rsync
+
+# Define source and destination
+SOURCE="/Volumes/LucidLinkFileSpace/"
+DESTINATION="/Volumes/Backup/Folder"
+LOG=/var/log/rsync.LucidLink.log.$(date -u ''+%y-%m-%d'') 
+
+date > "$LOG"
+
+# Check if the volume is mounted
+if mount | grep -q "Lucid@lucidfs0"; then
+    # Check if it's mounted with specific options
+    if mount | grep "Lucid@lucidfs0" | grep -q "lucidfs, local, synchronous"; then
+        # Execute rsync command with logging
+        rsync -avP --exclude={'*.mxf','*.MXF','*.MOV','*.mov','*.MP4','*.mp4'} "$SOURCE" "$DESTINATION" >> "$LOG" 2>&1
+    else
+        echo "Volume is mounted but does not have the expected options."
+    fi
+else
+    echo "Volume is not mounted."
+fi
